883fdb4e7c5806004673619da16e0a1a669065ee
Surya Sai Krishna <suryasada222@gmail.com>
2025-09-14 00:49:44 +0530

    done backend for register,login,logout

--- backend/index.js
+++ backend/index.js
@@ -40,17 +40,58 @@ app.post("/api/v1/register",async (req,res)=>{
             return res.json({success:false,msg:"user already exists"});
         }
 
-        const hashedpassword=bcrypt(password);
+        const hashedpassword=await bcrypt.hash(password,10);
         const newuser=await users.create({name,email,password:hashedpassword});
-        res.json(newuser);
+        
 
-        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
+        const token=jwt.sign({id:newuser._id},process.env.JWT_SECRET,{expiresIn:"7d"});
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.Node_env ==="production",
-            sameSite: process.env.Node_env === "production"?"none":"strict",
-            maxage: 7*24*60*60*1000
+            secure:process.env.NODE_ENV ==="production",
+            sameSite: process.env.NODE_ENV === "production"?"none":"strict",
+            maxAge: 7*24*60*60*1000
         });
+
+        return res.json({success:true,msg:"user registered successfully"});
+    }
+    catch(err){
+        res.json({success:false,msg:err.message});
+    }
+
+    
+
+})
+
+
+app.post("/api/v1/login",async (req,res)=>{
+    const {email,password}=req.body;
+
+    if(!email||!password){
+        res.json({success:false,msg:"enter valid credentials"});
+    }
+
+    
+
+    try{
+        const emailexists=await users.findOne({email});
+        if(!emailexists){
+            return res.json({success:false,msg:"invalid email"});
+        }
+
+        const ismatch=await bcrypt.compare(password,emailexists.password);
+        if(!ismatch){
+            return res.json({success:false,msg:"invalid password"});
+        }
+
+        const token=jwt.sign({id:emailexists._id},process.env.JWT_SECRET,{expiresIn:"7d"});
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV ==="production",
+            sameSite: process.env.NODE_ENV === "production"?"none":"strict",
+            maxAge: 7*24*60*60*1000
+        });
+
+        return res.json({success:true,msg:"user loggedin successfully"});
     }
     catch(err){
         res.json({success:false,msg:err.message});
@@ -58,6 +99,22 @@ app.post("/api/v1/register",async (req,res)=>{
 
 })
 
+app.post("/api/v1/logout",async (req,res)=>{
+
+    try{
+        res.clearCookie("token",{
+            httpOnly:true,
+            secure:process.env.NODE_ENV ==="production",
+            sameSite: process.env.NODE_ENV === "production"?"none":"strict",
+            maxAge: 7*24*60*60*1000
+        })
+
+        res.json({success:true,msg:"user logged out successfully"});
+    }
+    catch(err){
+        res.json({success:false,msg:err.message});
+    }
+})
 
 //----------------------------------------------------------listener-------------------------------------------------------
 app.listen(4000,()=>{
